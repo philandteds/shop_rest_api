400e9af7f084450014ab885607fae64f4e278650
Geoff Bentley <geoff@philandteds.com>
2013-03-22 16:17:11 +1300

    updated getStock method for PT
    - set LongCode as only parameter

--- classes/controllers/shop.php
+++ classes/controllers/shop.php
@@ -261,10 +261,7 @@ class ShopController extends ezpRestMvcController
 
 	public function doGetProductStock() {
 		$params = array(
-			'product_id' => false,
-			'region'     => false,
-			'colour'     => false,
-			'size'       => false
+			'longcode' => false
 		);
 
 		foreach( $params as $param => $value ) {
@@ -275,17 +272,12 @@ class ShopController extends ezpRestMvcController
 			}
 		}
 
-		$shop_ini = eZINI::instance( 'shop.ini' );
-		$warehouses = $shop_ini->variable( 'WarehouseSettings', 'AvailableWarehouses' );
 		$db = eZDB::instance();
 		$q  = '
 			SELECT InStock
 			FROM product
 			WHERE
-				LOWER( SUBSTRING_INDEX( LongCode, \'_\', -1 ) ) = LOWER( \'' . $db->escapeString( $warehouses[0] ) . '\' )
-				AND LOWER( ItemNumber ) = LOWER( \'' . $db->escapeString( $params['product_id'] ) . '\' )
-				AND LOWER( Series ) = LOWER( \'' . $db->escapeString( $params['size'] ) . '\' )
-				AND LOWER( Colour ) = LOWER( \'' . $db->escapeString( $params['colour'] ) . '\' );
+				LOWER( LongCode ) = LOWER( \'' . $db->escapeString( $params['longcode'] ) . '\' );
 		';
 		eZDebug::writeDebug($q, 'quantityCheck sql');
 		$r = $db->arrayQuery( $q );
