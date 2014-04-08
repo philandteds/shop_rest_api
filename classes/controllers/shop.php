e63b51d654737d560c22d00b993257789f013f0e
Geoff Bentley <geoff@philandteds.com>
2014-04-08 17:18:34 +1200

    set Eta to GMT time for getStock

--- classes/controllers/shop.php
+++ classes/controllers/shop.php
@@ -360,10 +360,10 @@ class ShopController extends ezpRestMvcController
 		if( count( $r ) === 0 ) {
 			throw new Exception( 'Product not found' );
 		}
-
+		$gmt_date = ($r[0]['Eta'] != '') ? DateTime::createFromFormat('d/m/Y', $r[0]['Eta'], new DateTimeZone('GMT')) : '';
 		$result = new ezpRestMvcResult();
 		$result->variables['stock_level'] = $r[0]['InStock'];
-		$result->variables['eta']         = $r[0]['Eta'];
+		$result->variables['eta']         = $gmt_date->format('Y-m-d H:i:sP');;
 		return $result;
 	}
 
